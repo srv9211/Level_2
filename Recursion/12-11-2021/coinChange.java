70ff789639c5e104e0c2f72ffb8954f07c12c92f
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2021-11-12 11:13:06 +0530

    Update coinChange.java

--- Recursion/12-11-2021/coinChange.java
+++ Recursion/12-11-2021/coinChange.java
@@ -11,11 +11,14 @@ public class Main{
         }
         int amount = scn.nextInt();
         
-        System.out.println(infi_combinations1(coins, amount, 0, ""));
+        // System.out.println(infi_combinations1(coins, amount, 0, ""));
         // System.out.println(infi_combinations2(coins, amount, 0, ""));
         // System.out.println(sin_combinations1(coins, amount, 0, ""));
         // System.out.println(sin_combinations2(coins, amount, 0, ""));
         // System.out.println(infi_permutations1(coins, amount, ""));
+        // System.out.println(infi_permutations2(coins, amount, 0, ""));
+        // System.out.println(sin_permutations1(coins, amount, ""));
+        System.out.println(sin_permutations2(coins, amount,0 , ""));
     }
     
     //========================================COMBINATION
@@ -80,63 +83,71 @@ public class Main{
     
     //========================================PERMUTATIONS
     
-    // public static int infi_permutations1(int[] coins, int amount, String psf){
+    public static int infi_permutations1(int[] coins, int amount, String psf){
         
-    //     if(amount==0){
-    //         System.out.println(psf);
-    //         return 1;
-    //     }
-    //     int count=0;
-    //     for(int i=0; i<coins.length; i++){
-    //         if(amount-coins[i]>=0)
-    //             count += infi_permutations1(coins, amount-coins[i], psf+coins[i]+" ");
-    //     }
-    //     return count;
-    // }
+        if(amount==0){
+            System.out.println(psf);
+            return 1;
+        }
+        int count=0;
+        for(int i=0; i<coins.length; i++){
+            if(amount-coins[i]>=0)
+                count += infi_permutations1(coins, amount-coins[i], psf+coins[i]+" ");
+        }
+        return count;
+    }
     
-    // public static int infi_combinations2(int[] coins, int amount, int idx, String psf){
+    public static int infi_permutations2(int[] coins, int amount, int idx, String psf){
         
-    //     if(amount==0){
-    //         System.out.println(psf);
-    //         return 1;
-    //     }
-    //     if(idx==coins.length)
-    //         return 0;
-    //     int count=0;
-    //     if(amount-coins[idx]>=0)
-    //         count += infi_combinations2(coins, amount-coins[idx], idx, psf+coins[idx]+" ");
-    //     count += infi_combinations2(coins, amount, idx+1, psf);
-    //     return count;
-    // }
+        if(amount==0){
+            System.out.println(psf);
+            return 1;
+        }
+        if(idx==coins.length)
+            return 0;
+        int count=0;
+        if(amount-coins[idx]>=0)
+            count += infi_permutations2(coins, amount-coins[idx], 0, psf+coins[idx]+" ");
+        count += infi_permutations2(coins, amount, idx+1, psf);
+        return count;
+    }
     
-    // public static int sin_combinations1(int[] coins, int amount, int idx, String psf){
+    public static int sin_permutations1(int[] coins, int amount, String psf){
         
-    //     if(amount==0){
-    //         System.out.println(psf);
-    //         return 1;
-    //     }
-    //     int count=0;
-    //     for(int i=idx; i<coins.length; i++){
-    //         if(amount-coins[i]>=0)
-    //             count += sin_combinations1(coins, amount-coins[i], i+1, psf+coins[i]+" ");
-    //     }
-    //     return count;
-    // }
+        if(amount==0){
+            System.out.println(psf);
+            return 1;
+        }
+        int count=0;
+        for(int i=0; i<coins.length; i++){
+            int ele = coins[i];
+            if(coins[i]>0 && amount-coins[i]>=0){
+                coins[i]=-coins[i];
+                count += sin_permutations1(coins, amount-ele, psf+ele+" ");
+                coins[i]=-coins[i];
+            }
+        }
+        return count;
+    }
     
-    // public static int sin_combinations2(int[] coins, int amount, int idx, String psf){
+    public static int sin_permutations2(int[] coins, int amount, int idx, String psf){
         
-    //     if(amount==0){
-    //         System.out.println(psf);
-    //         return 1;
-    //     }
-    //     if(idx==coins.length)
-    //         return 0;
-    //     int count=0;
-    //     if(amount-coins[idx]>=0)
-    //         count += sin_combinations2(coins, amount-coins[idx], idx+1, psf+coins[idx]+" ");
-    //     count += sin_combinations2(coins, amount, idx+1, psf);
-    //     return count;
-    // }
+        if(amount==0){
+            System.out.println(psf);
+            return 1;
+        }
+        if(idx==coins.length)
+            return 0;
+        int count=0;
+        int ele = coins[idx];
+        if(amount-coins[idx]>=0 && coins[idx]>0){
+            coins[idx]=-coins[idx];
+            count += sin_permutations2(coins, amount-ele, 0, psf+ele+" ");
+            coins[idx]=-coins[idx];
+        }
+        count += sin_permutations2(coins, amount, idx+1, psf);
+        return count;
+    }
     
 }
 
